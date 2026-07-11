90a9c22f2d6daba63131b64525c82e5763a43e51
i543999 <t.henke@sap.com>
2026-07-11 14:21:01 +0200

    extend middleware

--- code/01-http-api/internal/items/middleware.go
+++ code/01-http-api/internal/items/middleware.go
@@ -1,13 +1,16 @@
 package items
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+	"time"
 )
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("request", "method", r.Method, "path", r.URL.Path)
+		corrId := fmt.Sprintf("%d", time.Now().UnixNano())
+		slog.Info("request", "correlation_id", corrId, "method", r.Method, "path", r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
