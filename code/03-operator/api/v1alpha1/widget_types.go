571ba1980be10ca5b7904d8f7b90aecc4ac58e40
i543999 <t.henke@sap.com>
2026-07-11 22:02:42 +0200

    cleanup for easier learning

--- code/03-operator/api/v1alpha1/widget_types.go
+++ code/03-operator/api/v1alpha1/widget_types.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
@@ -21,38 +5,14 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // WidgetSpec defines the desired state of Widget
 type WidgetSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of Widget. Edit widget_types.go to remove/update
 	// +optional
 	Foo *string `json:"foo,omitempty"`
 }
 
 // WidgetStatus defines the observed state of Widget.
 type WidgetStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the Widget resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
 	// +listType=map
 	// +listMapKey=type
 	// +optional
@@ -64,19 +24,10 @@ type WidgetStatus struct {
 
 // Widget is the Schema for the widgets API
 type Widget struct {
-	metav1.TypeMeta `json:",inline"`
-
-	// metadata is a standard object metadata
-	// +optional
+	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitzero"`
-
-	// spec defines the desired state of Widget
-	// +required
-	Spec WidgetSpec `json:"spec"`
-
-	// status defines the observed state of Widget
-	// +optional
-	Status WidgetStatus `json:"status,omitzero"`
+	Spec              WidgetSpec   `json:"spec"`
+	Status            WidgetStatus `json:"status,omitzero"`
 }
 
 // +kubebuilder:object:root=true
