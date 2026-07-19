29b7f576df2a3601e7f479eb7ca94c8c300fe8f9
i543999 <t.henke@sap.com>
2026-07-19 15:49:21 +0200

    env to disable auth for dev purpose

--- code/01-http-api/internal/items/auth_middleware.go
+++ code/01-http-api/internal/items/auth_middleware.go
@@ -2,11 +2,18 @@ package items
 
 import (
 	"net/http"
+	"os"
 	"strings"
 )
 
 func AuthMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// disable authentication for local development
+		if os.Getenv("AUTH_DISABLED") == "true" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
 		var prefix string = "Bearer "
 		authHeader := r.Header.Get("Authorization")
 		if !strings.HasPrefix(authHeader, prefix) {
